0efb61ce80af4ecffd223a8ad62200978422af5a
TheClashFruit <clash.fruit123@gmail.com>
2021-08-29 19:52:50 +0200

    feat: add config.yml

--- src/main/java/xyz/theclashfruit/minej/Minej.java
+++ src/main/java/xyz/theclashfruit/minej/Minej.java
@@ -4,17 +4,28 @@ import delight.nashornsandbox.NashornSandbox;
 import delight.nashornsandbox.NashornSandboxes;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
 
 public final class Minej extends JavaPlugin {
+
+    FileConfiguration serverConfig = this.getConfig();
 
     @Override
     public void onEnable() {
         getLogger().info("onEnable has been invoked!");
+
+        serverConfig.addDefault("sandBoxMaxMemory", 512);
+        serverConfig.options().copyDefaults(true);
+
+        saveConfig();
     }
 
     @Override
@@ -27,13 +38,14 @@ public final class Minej extends JavaPlugin {
         if (cmd.getName().equalsIgnoreCase("minej")) {
             sender.sendMessage("Minej Go Brrr!");
 
-            //ScriptEngine sEngine = new ScriptEngineManager().getEngineByName("nashorn");
-
             NashornSandbox sandBox = NashornSandboxes.create();
             sandBox.allow(MinejJavaInterface.class);
 
+            //sandBox.setMaxMemory(serverConfig.getInt("sandBoxMaxMemory"));
+
             try {
                 sandBox.eval("print('hello form js');");
+                getLogger().info((String) sandBox.eval("return MinejJavaInterface;"));
             } catch (ScriptException e) {
                 e.printStackTrace();
             }
